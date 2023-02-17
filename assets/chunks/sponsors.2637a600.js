3bafcffdcf69b9b6607afa01703ebeb91b1000f8
lemonnuu <lemonnuu@users.noreply.github.com>
2023-02-17 08:30:49 +0000

    deploy: 10dfab3c313a3ea604aeb3eceb55981484c96b0a

---
+++ assets/chunks/sponsors.2637a600.js
@@ -0,0 +1,1 @@
+import{r as a}from"../app.f3eebbf7.js";const s=a(),o=a(!1),t="https://sponsors.vuejs.org",e=async()=>{o.value||(o.value=!0,s.value=await(await fetch(`${t}/data.json`)).json())};export{t as b,s as d,e as l};
